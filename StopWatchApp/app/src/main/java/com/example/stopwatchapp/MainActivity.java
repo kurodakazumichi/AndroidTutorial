0b04baa9217ffc10d28d4165982064bb112a5f58
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 03:42:23 +0900

    ストップウォッチの処理実装

--- StopWatchApp/app/src/main/java/com/example/stopwatchapp/MainActivity.java
+++ StopWatchApp/app/src/main/java/com/example/stopwatchapp/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.stopwatchapp;
 
+
+import android.os.Handler;
 import android.os.SystemClock;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.text.SimpleDateFormat;
+
 public class MainActivity extends AppCompatActivity {
 
 
 	private long startTime;
+	private long elapsedTime = 0l;
+
 	private Button startButton;
 	private Button stopButton;
 	private Button resetButton;
 	private TextView timerLabel;
+
+	private Handler handler = new Handler();
+	private Runnable updateTimer;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +61,41 @@ public class MainActivity extends AppCompatActivity {
 	 */
 	public void startTimer(View view)
 	{
-		// システムが軌道してからの経過時間
+		// システムが軌道してからの経過時間(ミリ秒)
 		startTime = SystemClock.elapsedRealtime();
 
 		// 一定時間ごとに現在の経過時間を表示する
+		updateTimer = new Runnable() {
+			@Override
+			public void run() {
+				long t = SystemClock.elapsedRealtime() - startTime + elapsedTime;
+				SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS", Locale.US);
+				timerLabel.setText(sdf.format(t));
+
+				// 念のためremoveしてからpostDelayedする。
+				handler.removeCallbacks(updateTimer);
+				handler.postDelayed(updateTimer, 10);
+			}
+		};
+
+		// 10ミリ秒後にupdateTimerを呼ぶ
+		handler.postDelayed(updateTimer, 10);
 
 		// ボタンの状態を設定
 		setButtonState(false, true, false);
+	}
+
+	public void stopTimer(View view)
+	{
+		elapsedTime = SystemClock.elapsedRealtime() - startTime;
+		handler.removeCallbacks(updateTimer);
+		setButtonState(true, false, true);
+	}
+
+	public void resetTimer(View view)
+	{
+		timerLabel.setText("00:00.000");
+		elapsedTime = 0l;
+		setButtonState(true, false, false);
 	}
 }
