28ea466b0f357cc4d54be3094b198bd6a0c45415
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 01:59:08 +0900

    名前入力とエラーチェック実装

--- NameScoreApp/app/src/main/java/com/example/namescoreapp/MainActivity.java
+++ NameScoreApp/app/src/main/java/com/example/namescoreapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.namescoreapp;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void getScore(View view) {
+		// EditTextを取得
+		EditText myEditText = (EditText) findViewById(R.id.myEditText);
+
+		// EditTextの中身を取得
+		String myName = myEditText.getText().toString().trim();
+
+		// 中身を見て処理を変える
+		if (myName.equals("")) {
+			// エラー処理
+
+			// 1.Contentのエラー機能
+			myEditText.setError("Please enter your name!");
+
+			// 2.Toastを使ったエラー表示
+			//Toast.makeText(this, "Please enter your name!", Toast.LENGTH_LONG).show();
+
+			// 3.Dialogを使ったエラー表示
+			/*
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+			alertDialogBuilder
+					.setTitle("Error")
+					.setMessage("Please enter your name!")
+					.setPositiveButton("OK", null);
+
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
+			*/
+
+		} else {
+
+		}
+
 	}
 }
