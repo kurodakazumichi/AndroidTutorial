6382ddcfd5f6bf2d57c61af486fb4ea40b690757
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 02:06:46 +0900

    結果画面レイアウト追加

---
+++ NameScoreApp/app/src/main/java/com/example/namescoreapp/MyResult.java
@@ -0,0 +1,13 @@
+package com.example.namescoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyResult extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_result);
+	}
+}
