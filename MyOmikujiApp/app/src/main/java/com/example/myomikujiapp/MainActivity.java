544dfacbff3516e3c7a5f8c3872e5cec06539e83
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-17 21:26:37 +0900

    おみくじ処理を実装

--- MyOmikujiApp/app/src/main/java/com/example/myomikujiapp/MainActivity.java
+++ MyOmikujiApp/app/src/main/java/com/example/myomikujiapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myomikujiapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void getOmikuji(View view) {
+		// TextViewの取得
+		TextView myTextView = (TextView) findViewById(R.id.myTextView);
+
+		String[] results = {
+				"大吉", "吉", "凶"
+		};
+
+		// 乱数の生成
+		Random randomGenerator = new Random();
+		int num = randomGenerator.nextInt(results.length);
+
+		// 結果の表示
+		if (num == 0)
+			myTextView.setTextColor(Color.RED);
+		else
+			myTextView.setTextColor(Color.rgb(0, 0, 0));
+
+		myTextView.setText(results[num]);
 	}
 }
