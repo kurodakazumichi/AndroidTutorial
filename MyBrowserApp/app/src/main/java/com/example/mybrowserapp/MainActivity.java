a1f7e2f1707f2f97059f95b049ce52ef4ba77fa6
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 07:08:12 +0900

    ブラウザ表示部分を実装

--- MyBrowserApp/app/src/main/java/com/example/mybrowserapp/MainActivity.java
+++ MyBrowserApp/app/src/main/java/com/example/mybrowserapp/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.mybrowserapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String INITIAL_WEBSITE = "http://dotinstall.com";
+
+	WebView myWebView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		myWebView = (WebView)findViewById(R.id.myWebView);
+
+		// Javascriptを有効にする設定
+		myWebView.getSettings().setJavaScriptEnabled(true);
+
+		// アプリ内でページ遷移させるための設定
+		myWebView.setWebViewClient(new WebViewClient());
+
+		// 指定されたURLのページを表示する
+		myWebView.loadUrl(INITIAL_WEBSITE);
+
 	}
 
 	@Override
