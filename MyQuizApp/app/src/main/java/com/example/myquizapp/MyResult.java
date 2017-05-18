dfbb83a592a1d23b1869602aaffd892888167cc4
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 09:27:45 +0900

    クイズアプリの実装

---
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/MyResult.java
@@ -0,0 +1,26 @@
+package com.example.myquizapp;
+
+import android.content.Intent;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MyResult extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_result);
+
+		TextView resultText = (TextView)findViewById(R.id.resultText);
+		Intent intent = getIntent();
+		resultText.setText(intent.getStringExtra(MyQuiz.EXTRA_MYSCORE));
+	}
+
+	public void goBack(View view)
+	{
+		finish();
+	}
+}
