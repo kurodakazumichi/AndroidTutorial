8d2688abdb1f4229b7ae8d8e8f6c4003aafaf87d
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-18 22:31:05 +0900

    リストビュー表示

--- MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.mylistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		ListView myListView = (ListView) findViewById(R.id.myListView);
+
+		// データ作成
+		ArrayList<String> items = new ArrayList<>();
+		for (int i = 0; i < 30; ++i) {
+			items.add("item-" + i);
+		}
+
+		// アダプター作成
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this,
+				R.layout.list_view,
+				items
+		);
+
+		// リストビューに表示
+		myListView.setAdapter(adapter);
 	}
 }
