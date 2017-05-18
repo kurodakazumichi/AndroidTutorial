eaf928b4342304d2da29fc10a430576403afead5
kazumichi kuroda <kazumichi96@gmail.com>
2017-05-19 02:52:44 +0900

    複雑なリストビュー

--- MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.mylistview;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.LiveFolders;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +29,94 @@ public class MainActivity extends AppCompatActivity {
 		ListView myListView = (ListView) findViewById(R.id.myListView);
 
 		// データ作成
-		ArrayList<String> items = new ArrayList<>();
-		for (int i = 0; i < 30; ++i) {
-			items.add("item-" + i);
-		}
+		ArrayList<User> items = new ArrayList<>();
+
+		items.add(new User(R.mipmap.ic_launcher, "Kuroda", "大阪市"));
+		items.add(new User(R.mipmap.ic_launcher, "Hirata", "大阪市"));
+		items.add(new User(R.mipmap.ic_launcher, "Daniera", "宝塚市"));
 
 		// アダプター作成
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				this,
-				R.layout.list_view,
-				items
-		);
+		UserAdapter adapter = new UserAdapter(this, R.layout.list_view, items);
 
 		// リストビューに表示
+		myListView.setEmptyView(findViewById(R.id.emptyView));
 		myListView.setAdapter(adapter);
+	}
+
+	public class UserAdapter extends ArrayAdapter<User> {
+		private LayoutInflater layoutInflater;
+
+		public UserAdapter(Context c, int id, ArrayList<User> users) {
+			super(c, id, users);
+			this.layoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public View getView(int pos, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = layoutInflater.inflate(R.layout.list_view, parent, false);
+
+				holder = new ViewHolder();
+				holder.icon = ((ImageView) convertView.findViewById(R.id.icon));
+				holder.name = ((TextView) convertView.findViewById(R.id.name));
+				holder.loc = ((TextView) convertView.findViewById(R.id.loc));
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder)convertView.getTag();
+			}
+
+			User user = (User) getItem(pos);
+
+			holder.icon.setImageBitmap(user.getIcon());
+			holder.name.setText(user.getName());
+			holder.loc.setText(user.getLoc());
+			return convertView;
+		}
+	}
+
+	static class ViewHolder {
+		ImageView icon;
+		TextView name;
+		TextView loc;
+	}
+
+	/**
+	 * ユーザーデータをまとめたクラス
+	 */
+	public class User {
+		private Bitmap icon;
+		private String name;
+		private String loc;
+
+		public User(int icon, String name, String loc) {
+			this.icon = BitmapFactory.decodeResource(getResources(), icon);
+			this.name = name;
+			this.loc = loc;
+		}
+
+		public Bitmap getIcon() {
+			return icon;
+		}
+
+		public void setIcon(Bitmap icon) {
+			this.icon = icon;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getLoc() {
+			return loc;
+		}
+
+		public void setLoc(String loc) {
+			this.loc = loc;
+		}
 	}
 }
